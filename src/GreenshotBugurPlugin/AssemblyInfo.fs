e2f18ccd396d2d8a7b857a78e495a0b3f6c5ac7f
Otto Gebb <otto.gebb@gmail.com>
2015-12-11 06:22:35 +0100

    Bump version to 1.1

--- src/GreenshotBugurPlugin/AssemblyInfo.fs
+++ src/GreenshotBugurPlugin/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("GreenshotBugurPlugin")>]
 [<assembly: AssemblyProductAttribute("GreenshotBugurPlugin")>]
 [<assembly: AssemblyDescriptionAttribute("A plugin for Greenshot that uploads images to Bugur.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("1.1")>]
+[<assembly: AssemblyFileVersionAttribute("1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "1.1"
